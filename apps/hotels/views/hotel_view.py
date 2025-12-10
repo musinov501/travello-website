681fd18927911554657b4c6f71401e5f7cc56105
Muhammadyor <musinovmuhammaader@gmail.com>
2025-12-10 18:30:15 +0500

    modified

--- apps/hotels/views/hotel_view.py
+++ apps/hotels/views/hotel_view.py
@@ -2,12 +2,20 @@
 from rest_framework import generics, permissions
 from apps.hotels.models import Hotel
 from apps.hotels.serializers.hotel_serializer import HotelSerializer, HotelListSerializer
+from apps.shared.utils.custom_response import CustomResponse
 
 
 class HotelListView(generics.ListAPIView):
     queryset = Hotel.objects.filter(is_available=True)
     serializer_class = HotelListSerializer
     permission_classes = [permissions.AllowAny]
+    
+    def list(self, request,  *args, **kwargs):
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return CustomResponse.success(
+            request=request,
+            data=serializer.data
+        )    
 
 
 
@@ -15,6 +23,18 @@ class HotelDetailView(generics.RetrieveAPIView):
     queryset = Hotel.objects.filter(is_available=True)
     serializer_class = HotelSerializer
     permission_classes = [permissions.AllowAny]
+    
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            hotel = self.get_object()
+        except Exception:
+            return CustomResponse.not_found(request=request)
+        
+        serializer = self.get_serializer(hotel)
+        return CustomResponse.success(
+            request=request,
+            data=serializer.data
+        )
 
 
 
@@ -22,6 +42,24 @@ class HotelCreateView(generics.CreateAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
     permission_classes = [permissions.IsAdminUser]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        
+        
+        if not serializer.is_valid():
+            return CustomResponse.validation_error(
+                request=request,
+                errors = serializer.errors
+            )
+        hotel =  serializer.save()
+        
+        return CustomResponse.success(
+            message_key="CREATED",
+            request=request,
+            data=HotelSerializer(hotel).data
+        )
+        
 
 
 
@@ -29,6 +67,8 @@ class HotelUpdateView(generics.UpdateAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
     permission_classes = [permissions.IsAdminUser]
+    
+    
 
 
 class HotelDeleteView(generics.DestroyAPIView):
