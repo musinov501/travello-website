bdf3f428dd1e82f597e47cf564d43a38dd50ccbe
Muhammadyor <musinovmuhammaader@gmail.com>
2025-12-10 14:53:44 +0500

    updated the tours app

--- apps/tours/views/tours_view.py
+++ apps/tours/views/tours_view.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from apps.tours.models.tours import Tour
 from apps.tours.serializers.tour_serializer import TourSerializer
+from apps.shared.utils.custom_response import CustomResponse
 
 
 class TourListView(generics.ListAPIView):
@@ -8,16 +9,55 @@ class TourListView(generics.ListAPIView):
     serializer_class = TourSerializer
     permission_classes = [permissions.AllowAny]
 
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        return CustomResponse.success(
+            message_key="SUCCESS_MESSAGE",
+            request=request,
+            data=serializer.data
+        )
+
 
 class TourDetailView(generics.RetrieveAPIView):
     queryset = Tour.objects.filter(status=True)
     serializer_class = TourSerializer
     permission_classes = [permissions.AllowAny]
 
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            tour = self.get_object()
+        except Exception:
+            return CustomResponse.not_found(request=request)
+
+        serializer = self.get_serializer(tour)
+        return CustomResponse.success(
+            message_key="SUCCESS_MESSAGE",
+            request=request,
+            data=serializer.data
+        )
+
+
 class TourCreateView(generics.CreateAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
     permission_classes = [permissions.IsAdminUser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            return CustomResponse.validation_error(
+                request=request,
+                errors=serializer.errors
+            )
+
+        tour = serializer.save()
+
+        return CustomResponse.success(
+            message_key="CREATED",
+            request=request,
+            data=TourSerializer(tour).data
+        )
 
 
 class TourUpdateView(generics.UpdateAPIView):
@@ -25,8 +65,47 @@ class TourUpdateView(generics.UpdateAPIView):
     serializer_class = TourSerializer
     permission_classes = [permissions.IsAdminUser]
 
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Exception:
+            return CustomResponse.not_found(request=request)
+
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True
+        )
+
+        if not serializer.is_valid():
+            return CustomResponse.validation_error(
+                request=request,
+                errors=serializer.errors
+            )
+
+        serializer.save()
+
+        return CustomResponse.success(
+            message_key="UPDATED",
+            request=request,
+            data=serializer.data
+        )
+
 
 class TourDeleteView(generics.DestroyAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
     permission_classes = [permissions.IsAdminUser]
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Exception:
+            return CustomResponse.not_found(request=request)
+
+        instance.delete()
+
+        return CustomResponse.success(
+            message_key="DELETED",
+            request=request
+        )
