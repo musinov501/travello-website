790694b9ec3ed863623eaf3c5da15ee1c8fa0f68
Muhammadyor <musinovmuhammaader@gmail.com>
2025-12-12 00:51:14 +0500

    modified the booking app

--- apps/bookings/urls/v1.py
+++ apps/bookings/urls/v1.py
@@ -2,9 +2,9 @@ from django.urls import path
 from apps.bookings.views.booking_view import BookingCreateView, BookingListView, BookingDetailView, BookingCancelView
 
 urlpatterns = [
-    path('', BookingListView.as_view(), name='booking-list-all'),  # all bookings
-    path('<str:booking_type>/', BookingListView.as_view(), name='booking-list-by-type'),  # filtered
     path("create/", BookingCreateView.as_view(), name="booking-create"),
-    path("<int:pk>/", BookingDetailView.as_view(), name="booking-detail"),
     path("<int:pk>/cancel/", BookingCancelView.as_view(), name="booking-cancel"),
+    path("<int:pk>/", BookingDetailView.as_view(), name="booking-detail"),
+    path('<str:booking_type>/', BookingListView.as_view(), name='booking-list-by-type'),
+    path('', BookingListView.as_view(), name='booking-list-all'),  
 ]
